8bf9e0e0dfefdf6acc97d917004270501b444f68
GeeMoose <moosemosse19@gmail.com>
2024-08-20 18:42:43 +0800

    delete selected option card

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 from utils.florence import load_florence_model, run_florence_inference, \
     FLORENCE_DETAILED_CAPTION_TASK, \
     FLORENCE_CAPTION_TO_PHRASE_GROUNDING_TASK, FLORENCE_OPEN_VOCABULARY_DETECTION_TASK
-from utils.sam import load_sam_image_model, run_sam_inference, load_sam_video_model
+from utils.sam import load_sam_image_model, run_sam_inference
 
 MARKDOWN = """
 # Florence2 + SAM2 🔥
@@ -36,9 +36,9 @@ object detection, image captioning, or phrase grounding. In the second stage, SA
 performs object segmentation on the image.
 """
 
-VIDEO_SCALE_FACTOR = 0.5
-VIDEO_TARGET_DIRECTORY = "tmp"
-create_directory(directory_path=VIDEO_TARGET_DIRECTORY)
+# VIDEO_SCALE_FACTOR = 0.5
+# VIDEO_TARGET_DIRECTORY = "tmp"
+# create_directory(directory_path=VIDEO_TARGET_DIRECTORY)
 
 DEVICE = torch.device("cuda")
 # DEVICE = torch.device("cpu")
@@ -51,7 +51,7 @@ if torch.cuda.get_device_properties(0).major >= 8:
 
 FLORENCE_MODEL, FLORENCE_PROCESSOR = load_florence_model(device=DEVICE)
 SAM_IMAGE_MODEL = load_sam_image_model(device=DEVICE)
-SAM_VIDEO_MODEL = load_sam_video_model(device=DEVICE)
+# SAM_VIDEO_MODEL = load_sam_video_model(device=DEVICE)
 COLORS = ['#FF1493', '#00BFFF', '#FF6347', '#FFD700', '#32CD32', '#8A2BE2']
 COLOR_PALETTE = sv.ColorPalette.from_hex(COLORS)
 BOX_ANNOTATOR = sv.BoxAnnotator(color=COLOR_PALETTE, color_lookup=sv.ColorLookup.INDEX)
@@ -80,56 +80,27 @@ def annotate_image(image, detections):
 @torch.inference_mode()
 @torch.autocast(device_type="cuda", dtype=torch.bfloat16)
 def process_image(
-    mode_dropdown, image_input, text_input
+    image_input, text_input
 ) -> Tuple[Optional[Image.Image], Optional[str]]:
     if not image_input:
         gr.Info("Please upload an image.")
         return None, None
 
-    if mode_dropdown == IMAGE_OPEN_VOCABULARY_DETECTION_MODE:
-        if not text_input:
-            gr.Info("Please enter a text prompt.")
-            return None, None
+    
+    if not text_input:
+        gr.Info("Please enter a text prompt.")
+        return None, None
 
-        texts = [prompt.strip() for prompt in text_input.split(",")]
-        detections_list = []
-        for text in texts:
-            _, result = run_florence_inference(
-                model=FLORENCE_MODEL,
-                processor=FLORENCE_PROCESSOR,
-                device=DEVICE,
-                image=image_input,
-                task=FLORENCE_OPEN_VOCABULARY_DETECTION_TASK,
-                text=text
-            )
-            detections = sv.Detections.from_lmm(
-                lmm=sv.LMM.FLORENCE_2,
-                result=result,
-                resolution_wh=image_input.size
-            )
-            detections = run_sam_inference(SAM_IMAGE_MODEL, image_input, detections)
-            detections_list.append(detections)
-
-        detections = sv.Detections.merge(detections_list)
-        detections = run_sam_inference(SAM_IMAGE_MODEL, image_input, detections)
-        return annotate_image(image_input, detections), None
-
-    if mode_dropdown == IMAGE_CAPTION_GROUNDING_MASKS_MODE:
+    texts = [prompt.strip() for prompt in text_input.split(",")]
+    detections_list = []
+    for text in texts:
         _, result = run_florence_inference(
             model=FLORENCE_MODEL,
             processor=FLORENCE_PROCESSOR,
             device=DEVICE,
             image=image_input,
-            task=FLORENCE_DETAILED_CAPTION_TASK
-        )
-        caption = result[FLORENCE_DETAILED_CAPTION_TASK]
-        _, result = run_florence_inference(
-            model=FLORENCE_MODEL,
-            processor=FLORENCE_PROCESSOR,
-            device=DEVICE,
-            image=image_input,
-            task=FLORENCE_CAPTION_TO_PHRASE_GROUNDING_TASK,
-            text=caption
+            task=FLORENCE_OPEN_VOCABULARY_DETECTION_TASK,
+            text=text
         )
         detections = sv.Detections.from_lmm(
             lmm=sv.LMM.FLORENCE_2,
@@ -137,8 +108,11 @@ def process_image(
             resolution_wh=image_input.size
         )
         detections = run_sam_inference(SAM_IMAGE_MODEL, image_input, detections)
-        return annotate_image(image_input, detections), caption
+        detections_list.append(detections)
 
+    detections = sv.Detections.merge(detections_list)
+    detections = run_sam_inference(SAM_IMAGE_MODEL, image_input, detections)
+    return annotate_image(image_input, detections), None
 
 # @torch.inference_mode()
 # @torch.autocast(device_type="cuda", dtype=torch.bfloat16)
@@ -300,7 +274,6 @@ with gr.Blocks() as demo:
     image_processing_submit_button_component.click(
         fn=process_image,
         inputs=[
-            image_processing_mode_dropdown_component,
             image_processing_image_input_component,
             image_processing_text_input_component
         ],
@@ -312,7 +285,6 @@ with gr.Blocks() as demo:
     image_processing_text_input_component.submit(
         fn=process_image,
         inputs=[
-            image_processing_mode_dropdown_component,
             image_processing_image_input_component,
             image_processing_text_input_component
         ],
--- utils/sam.py
+++ utils/sam.py
@@ -4,7 +4,7 @@ import numpy as np
 import supervision as sv
 import torch
 from PIL import Image
-from sam2.build_sam import build_sam2, build_sam2_video_predictor
+from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 
 SAM_CHECKPOINT = "checkpoints/sam2_hiera_small.pt"
